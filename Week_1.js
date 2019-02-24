66921ad33222f379c5ceaaf5d13f9c5c8f1d5b5f
Pavel Ilin <45457740+Refugee-Camp@users.noreply.github.com>
2019-02-24 13:04:47 -0500

    Update Week_1.js

--- Week_1.js
+++ Week_1.js
@@ -7,3 +7,16 @@ print(1, 2);
 #The line below prints one number and one string
 print(6, "hi");
 print("hello", 2, "bye");
+
+#Q1: Change the "print" code below so it produces the following output when run: 1 2 hello
+print(1, 2, "hello");
+
+#Q2: Change the "print" code below so it produces the following output when run:
+#1
+#1 2
+#1 2 3
+
+print(1);
+print(1,2);
+print(1,2,3);
+
