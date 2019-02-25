8ab2ef77ca6df2dcea8aa2a967b5dc2bfc46c423
Pavel Ilin <45457740+Refugee-Camp@users.noreply.github.com>
2019-02-25 11:39:58 -0500

    Update Week_1.js

--- Week_1.js
+++ Week_1.js
@@ -1,13 +1,3 @@
-#First Code Example - Print
-
-print(6);
-print(1, 2);
-
-#Print String
-#The line below prints one number and one string
-print(6, "hi");
-print("hello", 2, "bye");
-
 #Q1: Change the "print" code below so it produces the following output when run: 1 2 hello
 print(1, 2, "hello");
 
