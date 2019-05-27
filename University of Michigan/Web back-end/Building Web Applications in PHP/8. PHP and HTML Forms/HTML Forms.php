1358ca7219402f234e81743bc6e64e12d792e585
Refugee-Studio <refugeebase@gmail.com>
2019-05-27 11:43:28 -0400

    php

---
+++ University of Michigan/Web back-end/Building Web Applications in PHP/8. PHP and HTML Forms/HTML Forms.php
@@ -0,0 +1,12 @@
+<p>Some game... </p>
+<form>
+  <p><lable for="guess">Input Guess </lable>
+    <input type="text" name="guess" id="guess"/></p>
+    <input type="submit" />
+</form>
+<pre>
+  $_GET:
+  <?php
+  print_r($_GET);
+   ?>
+</pre>
