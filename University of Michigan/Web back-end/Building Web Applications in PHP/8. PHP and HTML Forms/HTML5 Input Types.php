2a587c424f37929dd90101428f6d1efbb3c9cb32
Refugee-Studio <refugeebase@gmail.com>
2019-05-28 10:39:50 -0400

    html5 input types

---
+++ University of Michigan/Web back-end/Building Web Applications in PHP/8. PHP and HTML Forms/HTML5 Input Types.php
@@ -0,0 +1,12 @@
+<input type="color" name="favcolor" value="#0000ff">
+
+<input type="date" name="bday" value="2019-05-28">
+
+<input type="email" name="email">
+
+<input type="number" name="quantity" min="1" max="5">
+
+<input type="url" name="homepage">
+
+## regular text field
+<input type="flying" name="saucer">
