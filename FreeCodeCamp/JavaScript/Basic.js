edee68cbc9261a1da7deef4e6992f9f3e49fd21b
pavel ilin <refugeebase@gmail.com>
2020-01-24 16:01:40 -0500

    JavaScript - Basics

---
+++ FreeCodeCamp/JavaScript/Basic.js
@@ -0,0 +1,19 @@
+// Appending Variables to Strings
+
+var someAdjective = 'test';
+var myStr = "Learning to code is ";
+myStr += someAdjective
+
+
+// Use Bracket Notation to Find the Nth Character in a String
+var lastName = "Lovelace";
+var thirdLetterOfLastName = lastName[2];
+
+// Use Bracket Notation to Find the Last Character in a String
+var lastName = "Lovelace";
+var lastLetterOfLastName = lastName[lastName.length - 1];
+
+// Use Bracket Notation to Find the Nth-to-Last Character in a String
+var lastName = "Lovelace";
+var secondToLastLetterOfLastName = lastName[lastName.length - 2];
+
