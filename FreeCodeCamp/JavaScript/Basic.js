2579543f90bed3a54da8bb145ea4c93294986e1c
pavel ilin <refugeebase@gmail.com>
2020-01-25 17:30:36 -0500

    javascript

--- FreeCodeCamp/JavaScript/Basic.js
+++ FreeCodeCamp/JavaScript/Basic.js
@@ -17,3 +17,37 @@ var lastLetterOfLastName = lastName[lastName.length - 1];
 var lastName = "Lovelace";
 var secondToLastLetterOfLastName = lastName[lastName.length - 2];
 
+// Selecting from Many Options with Switch Statements
+function caseInSwitch(val) {
+  var answer = "";
+  switch(val){
+    case 1:
+    answer = 'alpha';
+    break;
+
+    case 2:
+    answer = 'beta'
+    break;
+
+    case 3:
+    answer = 'gamma'
+    break;
+
+    case 4:
+    answer = 'delta'
+    break;
+
+    default:
+    answer = 'stuff'
+    break
+  }
+  return answer;
+}
+
+// Returning Boolean Values from Functions
+
+function isLess(a, b) {
+  return a < b
+}
+
+
