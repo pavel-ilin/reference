1fce7aed5cd34edb7ade086842a86628f50201b0
pavel ilin <refugeebase@gmail.com>
2020-01-26 18:33:34 -0500

    javascript

--- FreeCodeCamp/JavaScript/Basic.js
+++ FreeCodeCamp/JavaScript/Basic.js
@@ -50,4 +50,34 @@ function isLess(a, b) {
   return a < b
 }
 
+// Counting CardsPassed
+// In the casino game Blackjack, a player can gain an advantage over the house by keeping track of the relative number of high and low cards remaining in the deck. This is called Card Counting.
+// Having more high cards remaining in the deck favors the player. Each card is assigned a value according to the table below. When the count is positive, the player should bet high. When the count is zero or negative, the player should bet low.
+
+function cc(card) {
+  // Only change code below this line
+  switch (card) {
+    case 2:
+    case 3:
+    case 4:
+    case 5:
+    case 6:
+      count++;
+      break;
+    case 10:
+    case "J":
+    case "Q":
+    case "K":
+    case "A":
+      count--;
+      break;
+  }
+  if (count > 0) {
+    return count + " Bet";
+  } else {
+    return count + " Hold";
+  }
+  // Only change code above this line
+}
+cc(2); cc(3); cc(7); cc('K'); cc('A');
 
