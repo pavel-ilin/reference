e0ad4fe647b4d05fa7b6aae7079ac85cc15a1d04
pavel ilin <refugeebase@gmail.com>
2020-06-23 17:21:41 -0400

    algos

--- Algorithms/Add Two Numbers.js
+++ Algorithms/Add Two Numbers.js
@@ -12,19 +12,19 @@
  */
 
 const addTwoNumbers = (l1, l2) => {
-    let List = new ListNode(0);
-    let head = List;
+    let rootNode = new ListNode(0);
+    let node = rootNode;
     let sum = 0;
     let carry = 0;
 
-    while(l1 || l2 || sum > 0){
+    while(l1 || l2){
 
-        if(l1){
-            sum = sum + l1.val;
+        if (l1){
+            sum += l1.val;
             l1 = l1.next;
         }
-        if(l2){
-            sum = sum + l2.val;
+        if (l2){
+            sum += l2.val;
             l2 = l2.next;
         }
         if(sum > 9){
@@ -32,13 +32,12 @@ const addTwoNumbers = (l1, l2) => {
             sum = sum - 10;
         }
 
-        head.next = new ListNode(sum);
-        head = head.next;
+        node.next = new ListNode(sum);
+        node = node.next;
 
         sum = carry;
         carry = 0;
-
     }
 
-    return List.next;
+    return rootNode.next;
 };
